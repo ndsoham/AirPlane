a508ac73be78fd2780a530fe3af28a6711f5b5ab
N1v3x2 <kevzhang2022@gmail.com>
2023-07-23 11:52:27 -0500

    Finish File preview screen

---
+++ AirPlane/AirPlane/Views/FileInformationView.swift
@@ -0,0 +1,70 @@
+//
+//  FileInformationView.swift
+//  AirPlane
+//
+//  Created by Kevin Zhang on 7/23/23.
+//
+
+import SwiftUI
+
+struct FileInformationView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("File Name.ext")
+                .font(.title)
+            Text("218 KB")
+                .foregroundColor(.gray)
+                .padding(.bottom)
+            
+            Divider()
+            
+            Text("Information")
+                .font(.title)
+                .padding(.vertical, 10)
+            
+            VStack {
+                HStack {
+                    Text("Kind")
+                        .foregroundColor(.gray)
+                    Spacer()
+                    Text("Python Script")
+                }
+                Divider()
+                HStack {
+                    Text("Size")
+                        .foregroundColor(.gray)
+                    Spacer()
+                    Text("218 KB")
+                }
+                Divider()
+                HStack {
+                    Text("Created")
+                        .foregroundColor(.gray)
+                    Spacer()
+                    Text(Date.now, style: .date)
+                }
+                Divider()
+                HStack {
+                    Text("Modified")
+                        .foregroundColor(.gray)
+                    Spacer()
+                    Text(Date.now, style: .date)
+                }
+                Divider()
+                HStack {
+                    Text("Last opened")
+                        .foregroundColor(.gray)
+                    Spacer()
+                    Text(Date.now, style: .date)
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+struct FileInformationView_Previews: PreviewProvider {
+    static var previews: some View {
+        FileInformationView()
+    }
+}
